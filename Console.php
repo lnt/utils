0efe9e8b50f5c0a0717d4368eb25257f5f58178c
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-06-26 22:53:17 +0530

    Utils initd

---
+++ Console.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RudraX\Utils {
+
+	class Console {
+		public static function line() {
+			echo "\n###################################################################################################\n";
+		}
+		public static function log($msg) {
+			static $log_handle = null;
+			
+			$log = "[" . @date ( "Y-m-d H:i:s O" ) . "] " . $message . PHP_EOL;
+			if (defined ( "WRITE_TO_LOG" )) {
+				if ($log_handle === null) {
+					$log_handle = fopen ( WRITE_TO_LOG, 'a' );
+				}
+				
+				fwrite ( $log_handle, $log );
+			}
+			
+			echo $log;
+		}
+		public static function error($message) {
+			self::logmessage ( "ERROR: $message" );
+		}
+	}
+}
+
+?>
